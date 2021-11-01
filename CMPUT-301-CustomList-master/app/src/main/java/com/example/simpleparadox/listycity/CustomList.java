3534b4b3be5962c337b254b4d3ec7c1f1fba2c46
KexunNiu <77284970+KexunNiu@users.noreply.github.com>
2021-10-31 22:49:59 -0600

    Add files

---
+++ CMPUT-301-CustomList-master/app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -0,0 +1,50 @@
+package com.example.simpleparadox.listycity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class CustomList extends ArrayAdapter<City> {
+
+    private ArrayList<City> cities;
+    private Context context;
+
+    public CustomList(Context context, ArrayList<City> cities){
+        super(context,0, cities);
+        this.cities = cities;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//        return super.getView(position, convertView, parent);
+        View view = convertView;
+
+        if(view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.content, parent,false);
+        }
+
+        City city = cities.get(position);
+
+        TextView cityName = view.findViewById(R.id.city_text);
+        TextView provinceName = view.findViewById(R.id.province_text);
+
+        cityName.setText(city.getCityName());
+        provinceName.setText(city.getProvinceName());
+
+        return view;
+
+    }
+}
---
+++ CMPUT-301-CustomList-master/app/src/main/java/com/example/simpleparadox/listycity/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.simpleparadox.listycity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Declare the variables so that you will be able to reference it later.
+    ListView cityList;
+    ArrayAdapter<City> cityAdapter;
+    ArrayList<City> cityDataList;
+
+    CustomList customList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cityList = findViewById(R.id.city_list);
+
+        String []cities ={"Edmonton", "Vancouver", "Toronto", "Hamilton", "Denver", "Los Angeles"};
+        String []provinces = {"AB", "BC", "ON", "ON", "CO", "CA"};
+
+
+        cityDataList = new ArrayList<>();
+
+        for(int i=0;i<cities.length;i++){
+            cityDataList.add((new City(cities[i], provinces[i])));
+        }
+
+        cityAdapter = new CustomList(this, cityDataList);
+
+        cityList.setAdapter(cityAdapter);
+
+//        dataList = new ArrayList<>();
+//        dataList.addAll(Arrays.asList(cities));
+//
+//        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+//
+//        cityList.setAdapter(cityAdapter);
+
+
+
+    }
+
+
+}
